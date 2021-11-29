5323d812bf2c9474924b4e9af5328d0611ee575e
Omid Hajipoor <95117342+OmidHpr7@users.noreply.github.com>
2021-11-30 00:06:34 +0330

    Update CostFunction.py

--- CostFunction.py
+++ CostFunction.py
@@ -10,7 +10,7 @@
 import numpy as np
 
 
-def CF(x, F): 
+def CF(x, F): #[lb,ub,dim,fobj] 
 
     if F=='F1':
         o=sum(pow(x,2))
@@ -19,105 +19,48 @@ def CF(x, F):
         o=sum(abs(x))+np.prod(abs(x))
             
     elif F=='F3':
+        Le = x.shape
+        dim = Le[0]
         o=0;
-        for i in range(len(x)):
-            sm = sum(x[0:i])
-            o=o+pow(sm,2)
+        for i in range(dim+1):
+            o=o+pow(sum(x[0:i]),2)
             
     elif F=='F4':
-        o=max(abs(x))
+        o=max(abs(x));
             
-    #elif F=='F5':
-     #   dim=len(x)
-     #   o=sum(100*(x[1:dim]-pow(pow(x[0:dim-1],2),2))+pow((x[0:dim-1]-1),2))
+    elif F=='F5':
+        Le = x.shape
+        dim = Le[0]
+        o = sum((100*pow(((x[1:dim]-pow(x[0:dim-1],2))),2))+(pow((x[0:dim-1]-1),2)))
             
     elif F=='F6':
-        o=sum(pow(abs((x+.5)),2))
-        
-        
-    '''elif F=='F7':
-            lb=-1.28;
-            ub=1.28;
-            dim=30;
+        o=sum(abs(pow((x+0.5),2)))
+            
+    elif F=='F7':
+        Le = x.shape
+        dim = Le[0]
+        r=np.random.uniform(0,1,1)
+        temp =  list(range(1, dim+1)) 
+        o=sum((temp*pow(x,4)))+r
             
     elif F=='F8':
-            lb=-500;
-            ub=500;
-            dim=30;
+        o = sum(-x*(np.sin(np.sqrt(abs(x)))))
             
     elif F=='F9':
-            lb=-5.12;
-            ub=5.12;
-            dim=30;
+        Le = x.shape
+        dim = Le[0]
+        o = sum(pow(x,2)-(10*np.cos(2*np.pi*x))) + (10*dim)
             
     elif F=='F10':
-            lb=-32;
-            ub=32;
-            dim=30;
+        Le = x.shape
+        dim = Le[0]
+        o= (-20*np.exp(-0.2*np.sqrt(sum(pow(x,2))/dim)))-(np.exp(sum(np.cos(2*np.pi*x))/dim))+20+np.exp(1)
             
     elif F=='F11':
-            lb=-600;
-            ub=600;
-            dim=30;
-            
-    elif F=='F12':
-            lb=-50;
-            ub=50;
-            dim=30;
-            
-    elif F=='F13':
-            lb=-50;
-            ub=50;
-            dim=30;
-            
-    elif F=='F14':
-            lb=-65.536;
-            ub=65.536;
-            dim=2;
-            
-    elif F=='F15':
-            lb=-5;
-            ub=5;
-            dim=4;
-            
-    elif F=='F16':
-            lb=-5;
-            ub=5;
-            dim=2;
-            
-    elif F=='F17':
-            lb=[-5,0];
-            ub=[10,15];
-            dim=2;
-            
-    elif F=='F18':
-            lb=-2;
-            ub=2;
-            dim=2;
-            
-    elif F=='F19':
-            lb=0;
-            ub=1;
-            dim=3;
-            
-    elif F=='F20':
-            lb=0;
-            ub=1;
-            dim=6;     
-            
-    elif F=='F21':
-            lb=0;
-            ub=10;
-            dim=4;    
-            
-    elif F=='F22':
-            lb=0;
-            ub=10;
-            dim=4;    
-            
-    elif F=='F23':
-            lb=0;
-            ub=10;
-            dim=4;     '''       
+        Le = x.shape
+        dim = Le[0]
+        temp =  list(range(1, dim+1))
+        o = (sum(pow(x,2))/4000)-np.prod(np.cos(x/np.sqrt(temp)))+1
+                
     
     return o
