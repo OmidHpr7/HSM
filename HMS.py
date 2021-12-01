57f2e557e29fc1295ff0f79a17c7f27836c0751f
Omid Hajipoor <95117342+OmidHpr7@users.noreply.github.com>
2021-12-01 21:40:39 +0330

    Update HMS.py

--- HMS.py
+++ HMS.py
@@ -21,46 +21,46 @@ from sklearn.cluster import KMeans
 
 def HMS(nPop, MaxIt, VarMin, VarMax, NVar, F):
     #NVar=Params.nFeaturs;% Number of Variables
-    NPop=nPop       #Number of bids
-    K=5            #Number of cluster
-    C=2
-    MaxIter=MaxIt  #Maximum Iteration
-    ML=2           #Minimum mental process
-    MH=10          #Maximum mental precess
-    L=0.3          #lower bound
-    U=1.99         #Upper bound
+    NPop = nPop       #Number of bids
+    K = 5             #Number of cluster
+    C = 2
+    MaxIter = MaxIt   #Maximum Iteration
+    ML = 2            #Minimum mental process
+    MH = 10           #Maximum mental precess
+    L = 0.3           #lower bound
+    U = 1.99          #Upper bound
     
     
     
     
     #Initialization
 
-    X = initialization(NVar,NPop,VarMin,VarMax); # initialize a population of Npop bids
+    X = initialization(NVar,NPop,VarMin,VarMax);   # initialize a population of Npop bids
     X_Star = np.zeros((1,NVar))
-    Iter = 0;  # current iteration number
-    Cost_Bid =np.zeros((NPop,1))  # store fitness value for each Bid
-    Best_Iter=np.zeros((MaxIter,1)) #Best fittness for each Iteration
-    X_plus=np.zeros((1,NVar)) #best bid in the current Bids
-    Cost_X_Star=np.inf  #the best cost of bids in the initial population
-    Cost_X_plus=np.inf  #the best cost of bids in the current population
+    Iter = 0;                                      # current iteration number
+    Cost_Bid = np.zeros((NPop,1))                  # store fitness value for each Bid
+    Best_Iter = np.zeros((MaxIter,1))              # Best fittness for each Iteration
+    X_plus = np.zeros((1,NVar))                    # best bid in the current Bids
+    Cost_X_Star = np.inf                           # the best cost of bids in the initial population
+    Cost_X_plus = np.inf                           # the best cost of bids in the current population
     
     for i in range(NPop):
-        Cost_Bid[i,0] = CF(X[i,:], F) #Calculate the Cost of bids
+        Cost_Bid[i,0] = CF(X[i,:], F)             # Calculate the Cost of bids
         if  Cost_Bid[i,0] < Cost_X_Star:
-             Cost_X_Star= Cost_Bid[i,0]
-             X_Star=X[i,:]  #find the best bid in the initial population
+             Cost_X_Star = Cost_Bid[i,0]
+             X_Star = X[i,:]                        # find the best bid in the initial population
      
         
     X_Star = X_Star.reshape(1,NVar) 
-    NS=np.zeros((MH, NVar))
+    NS = np.zeros((MH, NVar))
     
     # Now start the Iteration
     while Iter < MaxIter:
         # Mental Search
         
         for i in range(NPop):
-            q=random.randint(ML,MH) #Produce new solutions with help mental search
-            beta=np.random.uniform(L,U) #Power law index  % Note: 0 < beta < 2
+            q = random.randint(ML,MH)               # Produce new solutions with help mental search
+            beta = np.random.uniform(L,U)           # Power law index  % Note: 0 < beta < 2
             MenSearch_Cost = []
             for i in range(q):
                 MenSearch_Cost.append(np.inf)
@@ -82,7 +82,7 @@ def HMS(nPop, MaxIt, VarMin, VarMax, NVar, F):
         cluster = kmeans.fit(X, K).labels_ 
         
         #MeanCost_Cluster= 100000*ones(C,1); 
-        Winner_Cluster_cost=np.inf
+        Winner_Cluster_cost = np.inf
         number_in_cluster = np.zeros((K,1))
         
         # Determine the number of members of each cluster
@@ -99,7 +99,7 @@ def HMS(nPop, MaxIt, VarMin, VarMax, NVar, F):
             Index_Cluster[k] = []
             for i in range(NPop):
                 if cluster[i]==k:
-                    counter=counter+1;
+                    counter = counter+1
                     Cost_Cluster[k].append(Cost_Bid[i][0])  
                     Index_Cluster[k].append(i)
             
@@ -125,21 +125,21 @@ def HMS(nPop, MaxIt, VarMin, VarMax, NVar, F):
         # Moving bids toward the best strategy
         for i in range(NPop):
             for n in range(NVar):
-                r=np.random.uniform(0,1,1);
+                r=np.random.uniform(0,1,1)
                 X[i,n]=X[i,n]+C*(r* Winner_bid[n]-X[i,n])
                 
             Cost_Bid[i] = CF(X[i,:], F);
-            if Cost_X_plus > Cost_Bid[i,0]: #% Find best bid in the current population
-                Cost_X_plus = Cost_Bid[i,0];
+            if Cost_X_plus > Cost_Bid[i,0]:       # Find best bid in the current population
+                Cost_X_plus = Cost_Bid[i,0]
                 X_plus=X[i,:]
                 
         X_plus = X_plus.reshape(1,NVar) 
         if Cost_X_Star > Cost_X_plus:
-            Cost_X_Star=Cost_X_plus
-            X_Star =X_plus
+            Cost_X_Star = Cost_X_plus
+            X_Star = X_plus
             
-        Best_Iter[Iter]=Cost_X_Star
-        Iter=Iter+1
+        Best_Iter[Iter] = Cost_X_Star
+        Iter = Iter+1
         print(Cost_X_Star)
         
 
